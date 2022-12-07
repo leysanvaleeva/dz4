980d81b76a0a233a86ac44f64ec08ae32566f9d8
leysan <leis90@list.ru>
2022-12-08 00:59:12 +0300

    3

--- zad29/Program.cs
+++ zad29/Program.cs
@@ -1,30 +1,30 @@
-﻿// Напишите программу, которая задаёт массив из 8 целых чисел с клавиатуры и далее выводит массив на экран в одну строку.
+﻿// // Напишите программу, которая задаёт массив из 8 целых чисел с клавиатуры и далее выводит массив на экран в одну строку.
 
 
 
-internal class Program
-{
-    private static void Main(string[] args)
-    {
-        int[] array = NewAr(size);
-        Console.WriteLine($"{NewAr(size)}");
+// internal class Program
+// {
+//     private static void Main(string[] args)
+//     {
+//         int[] array = NewAr(size);
+//         Console.WriteLine($"{NewAr(size)}");
 
 
 
 
-        int NewAr(int size)
-        {
-            for (int i = 0; i < 8; i++)
-            {
-                int[] array = new int[8];
-                Console.WriteLine($"введите число {i}: ");
-                array[i] = Convert.ToInt32(Console.ReadLine());
-            }
-            for (int k = 0; k < 8; k++)
-            {
-                Console.Write($"{array[k]");
-         Console.writeLine();
-    }
+//         int NewAr(int size)
+//         {
+//             for (int i = 0; i < 8; i++)
+//             {
+//                 int[] array = new int[8];
+//                 Console.WriteLine($"введите число {i}: ");
+//                 array[i] = Convert.ToInt32(Console.ReadLine());
+//             }
+//             for (int k = 0; k < 8; k++)
+//             {
+//                 Console.Write($"{array[k]");
+//          Console.writeLine();
+//     }
             
-}   НЕ ПОЛУЧИЛОСЬ!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
-}
+// }   НЕ ПОЛУЧИЛОСЬ!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+// }
